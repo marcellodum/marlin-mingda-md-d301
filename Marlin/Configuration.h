764f9d8442abc27604e907ad938eb883a2e1b8b2
Marcello Scacchetti <marcello.scacchetti@gmail.com>
2021-08-30 15:37:01 +0200

    default to 400mm/s for esteps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -748,7 +748,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.25, 79.25, 800, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.25, 79.25, 800, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
