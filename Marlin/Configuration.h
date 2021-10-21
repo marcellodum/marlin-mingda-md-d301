cfb51702759541f7b701a893ab9a84548130269a
Marcello Scacchetti <marcello.scacchetti@gmail.com>
2021-10-21 16:47:39 +0200

    model specific esteps settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -997,7 +997,11 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 400 }
+#if ENABLED(MINGDA_D2)
+  #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 400 }
+#else
+  #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 100 }
+#endif
 
 /**
  * Default Max Feed Rate (mm/s)
